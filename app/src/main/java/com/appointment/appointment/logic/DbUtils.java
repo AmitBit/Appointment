8a44fff826976e3692eab434d60c2918361523c0
Litalka <litalkapon@gmail.com>
2018-03-16 10:36:14 +0200

    Fixed git conflicts in DbUtils 2

--- app/src/main/java/com/appointment/appointment/logic/DbUtils.java
+++ app/src/main/java/com/appointment/appointment/logic/DbUtils.java
@@ -1,6 +1,10 @@
 package com.appointment.appointment.logic;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,6 +26,9 @@ public class DbUtils {
     public static final String DB_APPOINTMENT = "appointment";
 
     private static DbUtils dbUtils;
+
+
+    private static Boolean isFireBaseInitiated = false;
     private DatabaseReference reference;
 
     private DbUtils() {
@@ -30,17 +37,21 @@ public class DbUtils {
 
     // Need to lock
     public static DbUtils getInstance(){
-        if(dbUtils != null){
+        if(dbUtils == null){
             dbUtils = new DbUtils();
         }
 
         return dbUtils;
     }
 
+    public static Boolean getIsFireBaseInitiated() {
+        return isFireBaseInitiated;
+    }
+
     /*reference = FirebaseDatabase.getInstance().getReference();
     User user = new User("300481736", "Lital", "Kapon", "0508447486", "litalkapon@gmail.com");
         reference.child("users").push().setValue(user);*/
-    
+
     public void insertUser(User user){
         reference.child(DB_USER).push().setValue(user);
     }
@@ -57,6 +68,50 @@ public class DbUtils {
         reference.child(DB_APPOINTMENT).push().setValue(appointment);
     }
 
+    public void saveToSharedPref(Activity activity, String key, String data){
+        SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(key, data);
+        editor.commit();
+    }
+
+    public String readFromSharedPref(Activity activity, String key){
+        SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+        String result = sharedPref.getString(key, "");
+        return result;
+    }
+
+    public  void initFireBase() {
+        reference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(final DataSnapshot dataSnapshot) {
+                new AsyncTask<DataSnapshot, Void, Void>() {
+                    @Override
+                    protected Void doInBackground(DataSnapshot... dataSnapshots) {
+                        isFireBaseInitiated = true;
+                        for (DataSnapshot child: dataSnapshot.getChildren()) {
+                            Log.i("MyTag", child.getValue().toString());
+                            User user = child.getValue(User.class);
+                            Log.i("MyTag", user.getFirstName());
+                        }
+
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void aVoid) {
+
+                    }
+                }.execute(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     public List<Appointment> getBusinessAppointment(int businessId){
         reference.child(DB_APPOINTMENT).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -88,40 +143,4 @@ public class DbUtils {
 
         return null;
     }
-
-<<<<<<< HEAD
-    public List<Appointment> getBusinessAppointment(int businessId){
-        reference.child(DB_APPOINTMENT).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(final DataSnapshot dataSnapshot) {
-                new AsyncTask<DataSnapshot, Void, List<Appointment>>() {
-                    @Override
-                    protected List<Appointment> doInBackground(DataSnapshot... dataSnapshots) {
-                        List<Appointment> appointmentList = new ArrayList<>();
-                        for (DataSnapshot data: dataSnapshot.getChildren()) {
-                            Appointment appointment = data.getValue(Appointment.class);
-                            appointmentList.add(appointment);
-                            /*if(appointment.getKey().equals(Email.encodeID(email))){
-                                retrievedUser = messageSnapshot.getValue(User.class);
-                                break;
-                            }*/
-                        }
-
-                        return appointmentList;
-                    }
-
-                }.execute(dataSnapshot);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-        return null;
-    }
-=======
-
->>>>>>> origin/Lital_branch
 }
